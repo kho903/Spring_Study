3e6cb214d97e401af3c0f7610ffb86c1b1fff8aa
kho903 <gmldnr2222@naver.com>
2021-12-06 14:18:51 +0900

    JPA 기본 / 06. 다양한 연관관계 매핑 / 01. 다대일[N:1] - 다대일 양방향

--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,6 +16,9 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
