d3acefe9b9e6cc1380717fceecd2db2acc072917
kho903 <gmldnr2222@naver.com>
2021-12-06 13:57:25 +0900

    JPA 기본 / 05. 연관관계 매핑 기초 / 03. 실전 예제 2 - 연관관계 매핑 시작

--- JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +19,20 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // pseudo-code
+            // 1번 방법
+            /*Order order = new Order();
+            order.addOrderItem(new OrderItem());*/
+
+            // 2번 방법
+            Order order = new Order();
+            em.persist(order);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
+            // 핵심 : 될 수 있으면 단방향으로 해라.
+            // but, 실무에서 조회의 편의성을 위해 양방향을 쓸 때도 있음
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,13 +1,9 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -17,13 +13,22 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDate orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -33,12 +38,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDate getOrderDate() {
