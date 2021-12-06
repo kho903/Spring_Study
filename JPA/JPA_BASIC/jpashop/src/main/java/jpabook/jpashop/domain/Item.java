45cf85f6c29aa9a15a9c28b904a00b130159f17d
kho903 <gmldnr2222@naver.com>
2021-12-06 16:53:52 +0900

    JPA 기본 / 06. 다양한 연관관계 매핑 / 05. 실전 예제 3 - 다양한 연관관계 매핑

--- JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ JPA/JPA_BASIC/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -16,6 +19,9 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
     public Long getId() {
         return id;
