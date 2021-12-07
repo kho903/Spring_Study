82447e648ae5994b33cf72b47e40be08d256e65a
kho903 <gmldnr2222@naver.com>
2021-12-07 13:11:56 +0900

    JPA 기본 / 07. 고급 매핑 / 02. @MappedSuperclass - 매핑 정보 상속

--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -16,20 +17,16 @@ public class JpaMain {
 
         // code
         try {
+            Member member = new Member();
+            member.setUsername("Lee");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            Movie movie = new Movie();
-            movie.setDirector("a");
-            movie.setActor("bbb");
-            movie.setName("바람과 함께 사라지다.");
-            movie.setPrice(10000);
+            em.persist(member);
 
-            em.persist(movie);
 
             em.flush();
             em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit(); // DB에 저장되는 때!, [트랜잭션] 커밋!
         } catch (Exception e) {
--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -26,7 +26,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue
