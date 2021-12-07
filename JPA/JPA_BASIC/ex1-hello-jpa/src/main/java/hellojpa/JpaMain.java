52592917b196247ec428cf5a7038b2494c97ac8e
kho903 <gmldnr2222@naver.com>
2021-12-07 20:07:04 +0900

    JPA 기본 / 09. 값 타입 / 05. 값 타입 컬렉션

--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,12 +1,9 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,27 +15,49 @@ public class JpaMain {
 
         // code
         try {
-            Address address = new Address("city", "street", "100");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("족발");
+
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
             em.persist(member);
 
-            // 값 타입 복사 - 값 타입 공유 side effect 해결
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+            em.flush();
+            em.clear();
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
+//            System.out.println("============== START =================="); // 조회
+//            Member findMember = em.find(Member.class, member.getId());
+//
+//            List<Address> addressHistory = findMember.getAddressHistory();
+//            for (Address address : addressHistory) {
+//                System.out.println("address = " + address.getCity());
+//            }
+//
+//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+//            for (String favoriteFood : favoriteFoods) {
+//                System.out.println("favoriteFood = " + favoriteFood);
+//            }
 
-            // 값 타입 공유했을 때 side effect
-//            member.getHomeAddress().setCity("newCity"); // setter를 없앰
+            System.out.println("============== START =================="); // 수정
+            Member findMember = em.find(Member.class, member.getId());
 
-            // Setter를 없앤 뒤에 바꾸고 싶으면 Address를 새로 만든다.
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+////            findMember.getHomeAddress().setCity("new City"); // side effect 우려
+//            Address a = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("new City", a.getStreet(), a.getZipcode())); // 새로 넣을 것!
+
+//            // 치킨 -> 한식 (String은 update X -> remove 후 add)
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+            // remove 후 add (equals method로 같은 Address를 new 하여 그것과 같은 address를 지워주는 방식)
+            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit(); // DB에 저장되는 때!, [트랜잭션] 커밋!
         } catch (Exception e) {
--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,11 +2,19 @@ package hellojpa;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OrderColumn;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -19,13 +27,22 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    // Period
-    @Embedded
-    private Period workPeriod;
-
     // address
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -43,19 +60,27 @@ public class Member {
         this.username = username;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
