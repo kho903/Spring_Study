6b127516a4c9e13d5fe2bce939df802fec6edb43
kho903 <gmldnr2222@naver.com>
2021-12-07 19:11:13 +0900

    JPA 기본 / 09. 값 타입 / 03. 값 타입과 불변 객체

--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,27 @@ public class JpaMain {
 
         // code
         try {
+            Address address = new Address("city", "street", "100");
 
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "100"));
-            member.setWorkPeriod(new Period());
-
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            // 값 타입 복사 - 값 타입 공유 side effect 해결
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(copyAddress);
+            em.persist(member2);
+
+            // 값 타입 공유했을 때 side effect
+//            member.getHomeAddress().setCity("newCity"); // setter를 없앰
+
+            // Setter를 없앤 뒤에 바꾸고 싶으면 Address를 새로 만든다.
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
 
             tx.commit(); // DB에 저장되는 때!, [트랜잭션] 커밋!
         } catch (Exception e) {
--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -27,18 +27,6 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
-    // address
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city",
-                    column = @Column(name = "WORK_CITY")),
-            @AttributeOverride(name = "street",
-                    column = @Column(name = "WORK_STREET")),
-            @AttributeOverride(name = "zipcode",
-                    column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
-
     public Long getId() {
         return id;
     }
