96b62ee5e9d3b67d2647139ad4a9ad0ea66fdf87
kho903 <gmldnr2222@naver.com>
2021-12-07 19:23:11 +0900

    JPA 기본 / 09. 값 타입 / 04. 값 타입의 비교

--- JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ JPA/JPA_BASIC/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -2,7 +2,7 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-        int a = 10;
+        /*int a = 10;
         int b = a;
 
         a = 20;
@@ -15,6 +15,16 @@ public class ValueMain {
         // 있다면 변경하겠지만 변경 자체를 불가능하게 만들었다.
 
         System.out.println("aa = " + aa); // 10
-        System.out.println("bb = " + bb); // 10
+        System.out.println("bb = " + bb); // 10*/
+
+        int a = 10;
+        int b = 10;
+        System.out.println("(a==b) = " + (a == b)); // true
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("(address1 == address2) = " + (address1 == address2)); // false
+        System.out.println("(address1 equals address2) = " + (address1.equals(address2))); // 기본 false
+        // equals 를 오버라이드하면 true
     }
 }
