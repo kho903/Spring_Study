b438d05d05f4eb56e8f9ba0a7836dd114ef91973
kho903 <gmldnr2222@naver.com>
2021-12-08 23:31:46 +0900

    JPA 기본 / 10. 객체지향 쿼리 언어(JPQL) / 02. 중급 문법 / 06. Named 쿼리

--- JPA/JPA_BASIC/jpql/src/main/java/jpql/Member.java
+++ JPA/JPA_BASIC/jpql/src/main/java/jpql/Member.java
@@ -8,10 +8,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
