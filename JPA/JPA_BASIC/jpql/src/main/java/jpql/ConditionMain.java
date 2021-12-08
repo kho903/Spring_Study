bb4a0dd57f93773795243f32d716da2c914ae7a0
kho903 <gmldnr2222@naver.com>
2021-12-08 16:18:50 +0900

    JPA 기본 / 10. 객체지향 쿼리 언어(JPQL) / 01. 기본 문법 / 07. 조건식 (CASE 등등)

---
+++ JPA/JPA_BASIC/jpql/src/main/java/jpql/ConditionMain.java
@@ -0,0 +1,63 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class ConditionMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // code
+        try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("관리자");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+            member.setTeam(team);
+            em.persist(member);
+
+
+            em.flush();
+            em.clear();
+
+            // 기본 case 식
+            /*String query =
+                    "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >= 60 then '경로요금' " +
+                            "     else '학생요금' " +
+                            "end " +
+                    "from Member m";*/
+            // Coalesce
+            /*String query =
+                    "select coalesce(m.username, '이름 없는 회원') from Member m";*/
+            String query =
+                    "select nullif(m.username, '관리자') from Member m";
+            List<String> resultList = em.createQuery(query, String.class)
+                    .getResultList();
+
+            for (String s : resultList) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
