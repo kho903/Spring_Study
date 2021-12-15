21d0e9ba5257f61f2f9902792ad23c39cc6cd4d2
kho903 <gmldnr2222@naver.com>
2021-12-15 16:44:29 +0900

    Spring Data JPA / 03. 쿼리 메소드 기능 / 04. @Query, 값, DTO 조회하기

---
+++ JPA/SpringDataJpa/03. 쿼리 메소드 기능/04. @Query, 값, DTO 조회하기/README.md
@@ -0,0 +1,34 @@
+# @Query, 값, DTO 조회하기
+## 단순히 값 하나를 조회
+```java
+@Query("select m.username from Member m")
+List<String> findUsernameList();
+```
+- JPA 값 타입(`@Embedded`)도 이 방식으로 조회할 수 있다.
+
+## DTO로 직접 조회
+```java
+@Query("select new study.springdatajpa.dto.MemberDto(m.id, m.username, t.name) " +
+        "from Member m join m.team t")
+List<MemberDto> findMemberDto();
+```
+- 주의 : DTO로 직접 조회하려면 JPA `new` 명령어를 사용해야 한다. 그리고 다음과 같이
+생성자가 맞는 DTO가 필요하다. (JPA와 사용방식이 동일하다.)
+```java
+package study.datajpa.repository;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private Long id;
+    private String username;
+    private String teamName;
+
+    public MemberDto(Long id, String username, String teamName) {
+        this.id = id;
+        this.username = username;
+        this.teamName = teamName;
+    }
+}
+```
--- JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
+++ JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.entity.Member;
+import study.springdatajpa.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +20,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -105,5 +110,34 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findUser("AAA", 10);
         assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUsernameListTest() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+
+    @Test
+    public void findMemberDtoTest() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
