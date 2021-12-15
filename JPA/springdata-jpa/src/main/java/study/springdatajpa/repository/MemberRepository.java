23bafc5341809e4c6a35714a92153ff5551d2a60
kho903 <gmldnr2222@naver.com>
2021-12-15 16:58:06 +0900

    Spring Data JPA / 03. 쿼리 메소드 기능 / 05. 파라미터 바인딩

--- JPA/springdata-jpa/src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ JPA/springdata-jpa/src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.entity.Member;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -22,4 +23,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new study.springdatajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
 }
--- JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
+++ JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.entity.Member;
 import study.springdatajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -138,6 +139,19 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNamesTest() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : usernameList) {
+            System.out.println("member = " + member);
         }
     }
 }
