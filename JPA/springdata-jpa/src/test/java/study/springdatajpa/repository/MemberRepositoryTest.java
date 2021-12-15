96ced7ab973b98dbe9881e7b86700fc89d4af9c6
kho903 <gmldnr2222@naver.com>
2021-12-15 19:41:20 +0900

    Spring Data JPA / 03. 쿼리 메소드 기능 / 08. 스프링 데이터 JPA 페이징과 정렬

--- JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
+++ JPA/springdata-jpa/src/test/java/study/springdatajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,10 @@ package study.springdatajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.springdatajpa.dto.MemberDto;
@@ -184,5 +188,43 @@ class MemberRepositoryTest {
         Optional<Member> exceptionOptional = memberRepository.findOptionalByUsername("AAA");
         System.out.println("exceptionOptional = " + exceptionOptional); // Optional.empty
 
+    }
+
+
+    @Test
+    public void paging() {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        // when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+//        Slice<Member> slice = memberRepository.findByAge(age, pageRequest);
+//        List<Member> slice = memberRepository.findByAge(age, pageRequest);
+
+        Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+
+        // then
+        List<Member> content = page.getContent();
+//        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+
+/*//        assertThat(slice.getTotalElements()).isEqualTo(5);
+        assertThat(slice.getNumber()).isEqualTo(0);
+//        assertThat(slice.getTotalPages()).isEqualTo(2);
+        assertThat(slice.isFirst()).isTrue();
+        assertThat(slice.hasNext()).isTrue();*/
     }
 }
