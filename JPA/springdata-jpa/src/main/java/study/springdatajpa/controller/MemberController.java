a05f8da3f3eaa96a70f553184211219683cb56be
kho903 <gmldnr2222@naver.com>
2021-12-19 17:48:27 +0900

    Spring Data JPA / 04. 확장 기능 / 03. Web 확장 / 02. 페이징과 정렬

--- JPA/springdata-jpa/src/main/java/study/springdatajpa/controller/MemberController.java
+++ JPA/springdata-jpa/src/main/java/study/springdatajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.springdatajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.entity.Member;
 import study.springdatajpa.repository.MemberRepository;
 
@@ -26,8 +30,17 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(MemberDto::new);
+        return map;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
