2045df8e13a504d590849607ce65b0411f8ad238
kho903 <gmldnr2222@naver.com>
2021-12-21 19:50:43 +0900

    Query DSL / 04. 실무 활용 / 03. 스프링 데이터 페이징 활용 / 02. CountQuery 최적화

--- JPA/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ JPA/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,10 +2,12 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -70,9 +72,22 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .fetchResults();
 
         List<MemberTeamDto> content = results.getResults();
-        long total = results.getTotal();
+//        long total = results.getTotal();
 
-        return new PageImpl<>(content, pageable, total);
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                );
+
+
+//        return new PageImpl<>(content, pageable, total);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount)
     }
 
     @Override
