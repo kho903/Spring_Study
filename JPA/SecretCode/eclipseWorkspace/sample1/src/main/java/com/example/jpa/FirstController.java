346f3ac2a4518a8094c7df9c80a695a4f8d2acb3
kho903 <gmldnr2222@naver.com>
2022-01-04 21:50:58 +0900

    SecretCode-SpringBoot,JPA / 01. Spring RestController / 01. 주소 요청에 대한 이해 2. 클라이언트 요청에 대한 주소와 문자열을 리턴하는 함수를 작성 조건 - 컨트롤러 인식을 위한 Controller 어노테이션 이용 - 주소매핑은 RequestMapping을 이용 - HTTP 메소드는 GET 방식 - 요청 주소는 "/helloworld" - 리턴값은 "hello world" 문자열 리턴 - 주소는 "/helloworld"

--- JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/FirstController.java
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/FirstController.java
@@ -3,6 +3,7 @@ package com.example.jpa;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirstController {
@@ -10,5 +11,11 @@ public class FirstController {
 	@RequestMapping(value = "/first-url", method = RequestMethod.GET)
 	public void first() {
 		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/helloworld")
+	public String helloWorld() {
+		return "hello world";
 	}
 }
