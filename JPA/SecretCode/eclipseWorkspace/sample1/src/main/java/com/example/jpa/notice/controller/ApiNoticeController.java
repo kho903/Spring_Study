e29e7e4806f89386ec87183d3520f2239a47cd55
kho903 <gmldnr2222@naver.com>
2022-01-05 00:37:44 +0900

    secretCode-SpringBoot,JPA / 01. Spring RestController / 02. 게시판 기본 목록 8. 공지사항 게시판 목록에 대한 요청을 처리하는 API [조건] - REST API 형식으로 구현 - HTTP METHOD는 GET - 요청 주소는 "/api/notice" - 리턴값은 공지사항 게시판의 내용을 추상화한 모델(게시글ID, 제목, 내용, 등록일)이며 복수형태의 데이터 리턴    - 게시글ID = 1, 제목 = 공지사항입니다, 내용 = 공지사항 내용입니다, 등록일 = 2022-1-5    - 게시글ID = 2, 제목 = 두번째 공지사항입니다, 내용 = 두번째 공지사항 내용입니다, 등록일 = 2022-1-6

--- JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiNoticeController {
@@ -27,4 +29,43 @@ public class ApiNoticeController {
         notice.setRegDate(regDate);
         return notice;
     }
+
+    @GetMapping("/api/notice3")
+    public List<NoticeModel> noticeModelList() {
+        List<NoticeModel> noticeList = new ArrayList<>();
+
+        LocalDateTime regDate = LocalDateTime.of(2022, 1, 5, 0, 0);
+        LocalDateTime regDate2 = LocalDateTime.of(2022, 1, 6, 0, 0);
+
+        NoticeModel notice1 = new NoticeModel();
+        notice1.setId(1);
+        notice1.setTitle("공지사항입니다.");
+        notice1.setContents("공지사항 내용입니다.");
+        notice1.setRegDate(regDate);
+
+        /*
+        NoticeModel notice2 = new NoticeModel();
+        notice2.setId(2);
+        notice2.setTitle("두번째 공지사항입니다.");
+        notice2.setContents("두번쨰 공지사항 내용입니다.");
+        notice2.setRegDate(regDate2);*/
+
+        // builder 사용
+        /*NoticeModel notice2 = NoticeModel.builder()
+                .id(2)
+                .title("두번째 공지사항입니다.")
+                .contents("두번쨰 공지사항 내용입니다.")
+                .regDate(regDate2)
+                .build();*/
+
+        noticeList.add(notice1);
+        noticeList.add(NoticeModel.builder()
+                .id(2)
+                .title("두번째 공지사항입니다.")
+                .contents("두번쨰 공지사항 내용입니다.")
+                .regDate(regDate2)
+                .build());
+        return noticeList;
+    }
+
 }
