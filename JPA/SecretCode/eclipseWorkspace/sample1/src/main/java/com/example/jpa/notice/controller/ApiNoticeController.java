7d8a7dd331916dbd9dae40bb67c4888a99230fb4
kho903 <gmldnr2222@naver.com>
2022-01-05 20:25:27 +0900

    secretCode-SpringBoot,JPA / 01. Spring RestController / 04. 게시글 수정 20. 공지사항에 글의 조회수를 증가시키는 API [조건] - REST API 형식으로 구현 - HTTP METHOD는 PATCH - 요청 주소는 "/api/notice/{id}/hits" ({id}는 공지사항의 글ID로 동적으로 변함)

--- JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/notice/controller/ApiNoticeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -205,6 +206,15 @@ public class ApiNoticeController {
         notice.setUpdateDate(LocalDateTime.now());
         noticeRepository.save(notice);
 
+    }
+
+    @PatchMapping("/api/notice/{id}/hits")
+    public void noticeHits(@PathVariable Long id) {
+        Notice notice = noticeRepository.findById(id)
+                .orElseThrow(() -> new NoticeNotFoundException("공지사항에 글이 존재하지 않습니다."));
+
+        notice.setHits(notice.getHits() + 1);
+        noticeRepository.save(notice);
     }
 }
 
