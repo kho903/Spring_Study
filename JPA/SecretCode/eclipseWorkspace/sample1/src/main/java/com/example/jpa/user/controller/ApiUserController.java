b277cbacc2db6f0b317b5c8983810519642e36a4
kho903 <gmldnr2222@naver.com>
2022-01-07 14:53:57 +0900

    secretCode-SpringBoot,JPA / 02. Spring JPA를 통한 CRUD / 01. 사용자 기능 32. 사용자 정보를 입력받아서 저장하는 API - 입력값 : 이메일, 이름, 비밀번호, 연락처, 가입일 (현재일시)

--- JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/user/controller/ApiUserController.java
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/user/controller/ApiUserController.java
@@ -1,7 +1,10 @@
 package com.example.jpa.user.controller;
 
 import com.example.jpa.notice.model.ResponseError;
+import com.example.jpa.user.entity.User;
 import com.example.jpa.user.model.UserInput;
+import com.example.jpa.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -11,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ApiUserController {
+    private final UserRepository userRepository;
 
     @PostMapping("/api/user")
     public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
@@ -31,6 +37,32 @@ public class ApiUserController {
         }
 
 //        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/user2")
+    public ResponseEntity<?> addUser2(@RequestBody @Valid UserInput userInput, Errors errors) {
+
+        List<ResponseError> responseErrorList = new ArrayList<>();
+
+        if (errors.hasErrors()) {
+            errors.getAllErrors().forEach((e) -> {
+                responseErrorList.add(ResponseError.of((FieldError) e));
+            });
+
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+        }
+
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .userName(userInput.getUserName())
+                .password(userInput.getPassword())
+                .phone(userInput.getPhone())
+                .regDate(LocalDateTime.now())
+                .build();
+
+        userRepository.save(user);
+
         return ResponseEntity.ok().build();
     }
 }
---
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/user/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.jpa.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String password;
+
+    @Column
+    private String phone;
+
+    @Column
+    private LocalDateTime regDate;
+}
