5d772c1fff9364a83df7a60d5db095b992be1643
kho903 <gmldnr2222@naver.com>
2022-01-17 14:37:55 +0900

    secretCode-SpringBoot,JPA / 04. 로그 및 OPEN-API 연동 기능 / 02. 공공 API 활용 88. RestTemplate을 이용한 공공데이터포털의 공공API 연동하여 전국 약국 목록을 가져오는 API - 시도/구군 단위 검색기능에 대한 구현을 추가 - 결과 데이터를 모델로 매핑하여 처리

--- JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/extra/ApiExtraController.java
+++ JPA/SecretCode/eclipseWorkspace/sample1/src/main/java/com/example/jpa/extra/ApiExtraController.java
@@ -2,6 +2,7 @@ package com.example.jpa.extra;
 
 import com.example.jpa.common.model.ResponseResult;
 import com.example.jpa.extra.model.OpenApiResult;
+import com.example.jpa.extra.model.PharmacySearch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.Collections;
 
 @RestController
@@ -23,13 +26,13 @@ public class ApiExtraController {
     @GetMapping("/api/extra/pharmacy")
     public String pharmacy() {
 
-        String apyKey = "";
+        String apiKey = "";
 
         String url = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
 
         String apiResult = "";
         try {
-            URI uri = new URI(String.format(url, apyKey));
+            URI uri = new URI(String.format(url, apiKey));
 
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
@@ -48,13 +51,52 @@ public class ApiExtraController {
     @GetMapping("/api/extra/pharmacy/v2")
     public ResponseEntity<?> pharmacyV2() {
 
-        String apyKey = "";
+        String apiKey = "";
 
         String url = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
 
         String apiResult = "";
         try {
-            URI uri = new URI(String.format(url, apyKey));
+            URI uri = new URI(String.format(url, apiKey));
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            String result = restTemplate.getForObject(uri, String.class);
+            apiResult = result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        OpenApiResult jsonResult = null;
+        try {
+            jsonResult = objectMapper.readValue(apiResult, OpenApiResult.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseResult.success(jsonResult);
+    }
+
+    @GetMapping("/api/extra/pharmacy/v3")
+    public ResponseEntity<?> pharmacyV3(@RequestBody PharmacySearch pharmacySearch) {
+
+        String apiKey = "";
+
+        String url = String.format("http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10", apiKey);
+
+        String apiResult = "";
+        try {
+
+            url += String.format("&Q0=%s&Q1=%s",
+                    URLEncoder.encode(pharmacySearch.getSearchSido(), "UTF-8"),
+                    URLEncoder.encode(pharmacySearch.getSearchGugun(), "UTF-8")
+            );
+
+            URI uri = new URI(url);
 
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
