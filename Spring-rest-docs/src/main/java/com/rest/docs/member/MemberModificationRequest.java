cb5d861788de9cecefd555dc1cdad589b88ad2c8
kho903 <gmldnr2222@naver.com>
2022-01-25 00:57:51 +0900

    Spring REST Docs / API - create, read, update member

---
+++ Spring-rest-docs/src/main/java/com/rest/docs/member/MemberModificationRequest.java
@@ -0,0 +1,12 @@
+package com.rest.docs.member;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+public class MemberModificationRequest {
+    @NotEmpty
+    private String name;
+}
