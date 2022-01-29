6094a9075ba0b6c1f0ab30395dfbdd4d726dbf6b
kho903 <gmldnr2222@naver.com>
2022-01-29 20:25:17 +0900

    Spring REST Docs / member status 추가

--- Spring-rest-docs/src/main/java/com/rest/docs/DataSetup.java
+++ Spring-rest-docs/src/main/java/com/rest/docs/DataSetup.java
@@ -1,5 +1,8 @@
-package com.rest.docs.member;
+package com.rest.docs;
 
+import com.rest.docs.member.Member;
+import com.rest.docs.member.MemberRepository;
+import com.rest.docs.member.MemberStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -18,10 +21,10 @@ public class DataSetup implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         final List<Member> members = new ArrayList<>();
 
-        members.add(new Member("yun@bbb.com", "yun"));
-        members.add(new Member("jin@bbb.com", "jin"));
-        members.add(new Member("han@bbb.com", "han"));
-        members.add(new Member("jo@bbb.com", "jo"));
+        members.add(new Member("yun@bbb.com", "yun", MemberStatus.BAN));
+        members.add(new Member("jin@bbb.com", "jin", MemberStatus.NORMAL));
+        members.add(new Member("han@bbb.com", "han", MemberStatus.NORMAL));
+        members.add(new Member("jo@bbb.com", "jo", MemberStatus.LOCK));
 
         memberRepository.saveAll(members);
 
--- Spring-rest-docs/src/test/java/com/rest/docs/RestDocumentApi.java
+++ Spring-rest-docs/src/test/java/com/rest/docs/RestDocumentApi.java
@@ -1,6 +1,10 @@
 package com.rest.docs;
 
-import org.hibernate.annotations.BatchSize;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.rest.docs.member.MemberStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +13,42 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.EnumSet;
 
 @RestController
 @RequestMapping("/test")
 public class RestDocumentApi {
 
+    private final ObjectMapper mapper;
+
+    public RestDocumentApi(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
     @PostMapping("/sample")
     public void sample(@RequestBody @Valid SampleRequest dto) {
 
+    }
+
+    /**
+     * 1. Test Controller(Rest Docs 문서용)에 Member Status Enum Api를 생성
+     * 2. Test Controller에 대한 테스트 코드 작성 -> Member Status Enum snippet 파일 생성
+     * 3. Member Status Enum snippets 기반으로 Document 작성
+     */
+
+    @GetMapping("/memberStatus")
+    public ArrayNode getMemberStatus() {
+        final ArrayNode arrayNode = mapper.createArrayNode();
+        final EnumSet<MemberStatus> types = EnumSet.allOf(MemberStatus.class);
+
+        for (final MemberStatus type : types) {
+            final ObjectNode node = mapper.createObjectNode();
+            node.put("MemberStatus", type.name());
+            node.put("MemberStatus", type.getDescription());
+            arrayNode.add(node);
+        }
+
+        return arrayNode;
     }
 
     public static class SampleRequest {
