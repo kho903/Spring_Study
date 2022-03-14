fc7da9f0c3e045c84e8751251390d48fb4eac312
kho903 <gmldnr2222@naver.com>
2022-03-14 17:40:58 +0900

    Spring Core / 02. 스프링 웹 어플리케이션 핵심 기술 (예제) / 05. 비즈니스 validation 적용

--- SpringCore/dmaker/src/main/java/com/developers/dmaker/repository/DeveloperRepository.java
+++ SpringCore/dmaker/src/main/java/com/developers/dmaker/repository/DeveloperRepository.java
@@ -1,5 +1,7 @@
 package com.developers.dmaker.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.developers.dmaker.entity.Developer;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+	Optional<Developer> findByMemberId(String memberId);
+
 }
--- SpringCore/dmaker/src/main/java/com/developers/dmaker/service/DMakerService.java
+++ SpringCore/dmaker/src/main/java/com/developers/dmaker/service/DMakerService.java
@@ -1,10 +1,15 @@
 package com.developers.dmaker.service;
 
+import static com.developers.dmaker.exception.DMakerErrorCode.*;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.developers.dmaker.dto.CreateDeveloper;
 import com.developers.dmaker.entity.Developer;
+import com.developers.dmaker.exception.DMakerException;
 import com.developers.dmaker.repository.DeveloperRepository;
 import com.developers.dmaker.type.DeveloperLevel;
 import com.developers.dmaker.type.DeveloperSkillType;
@@ -18,6 +23,7 @@ public class DMakerService {
 
 	@Transactional
 	public void createDeveloper(CreateDeveloper.Request request) {
+		validateCreateDeveloperRequest(request);
 		Developer developer = Developer.builder()
 			.developerLevel(DeveloperLevel.JUNIOR)
 			.developerSkillType(DeveloperSkillType.FRONT_END)
@@ -27,5 +33,30 @@ public class DMakerService {
 			.build();
 
 		developerRepository.save(developer);
+	}
+
+	private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+		// business validation
+		DeveloperLevel developerLevel = request.getDeveloperLevel();
+		Integer experienceYears = request.getExperienceYears();
+		if (developerLevel == DeveloperLevel.SENIOR
+			&& experienceYears < 10) {
+			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+		}
+		if (developerLevel == DeveloperLevel.JUNGNIOR
+			&& experienceYears < 4 || experienceYears > 10) {
+			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+		}
+		if (developerLevel == DeveloperLevel.JUNIOR
+			&& experienceYears > 4) {
+			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+		}
+
+		// Optional<Developer> developer = developerRepository.findByMemberId(request.getMemberId());
+		// if (developer.isPresent()) throw new DMakerException(DUPLICATED_MEMBER_ID);
+		developerRepository.findByMemberId(request.getMemberId())
+			.ifPresent((developer -> {
+				throw new DMakerException(DUPLICATED_MEMBER_ID);
+			}));
 	}
 }
