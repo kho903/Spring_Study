5920c6717f375df6654524fd47271861f92b6c1d
kho903 <gmldnr2222@naver.com>
2022-03-21 02:17:27 +0900

    Spring Core / 06. 리팩토링 / 05. 리팩토링 진행 4

--- SpringCore/dmaker/src/main/java/com/developers/dmaker/service/DMakerService.java
+++ SpringCore/dmaker/src/main/java/com/developers/dmaker/service/DMakerService.java
@@ -1,8 +1,6 @@
 package com.developers.dmaker.service;
 
-import static com.developers.dmaker.constant.DMakerConstant.*;
 import static com.developers.dmaker.exception.DMakerErrorCode.*;
-import static com.developers.dmaker.type.DeveloperLevel.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -108,16 +106,8 @@ public class DMakerService {
 	private void validateDeveloperLevel(
 		DeveloperLevel developerLevel, Integer experienceYears
 	) {
-		if (developerLevel == SENIOR
-			&& experienceYears < MIN_SENIOR_EXPERIENCE_YEARS) {
-			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
-		}
-		if (developerLevel == DeveloperLevel.JUNGNIOR
-			&& (experienceYears < MAX_JUNIOR_EXPERIENCE_YEARS || experienceYears > MIN_SENIOR_EXPERIENCE_YEARS)) {
-			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
-		}
-		if (developerLevel == DeveloperLevel.JUNIOR
-			&& experienceYears > MAX_JUNIOR_EXPERIENCE_YEARS) {
+		if (experienceYears < developerLevel.getMinExperienceYears() ||
+			experienceYears > developerLevel.getMaxExperienceYears()) {
 			throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
 		}
 	}
