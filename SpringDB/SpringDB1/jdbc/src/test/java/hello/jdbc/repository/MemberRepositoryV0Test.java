1cbebb757d14d5b77b8c8a14a8f8991797dd3bb8
kho903 <gmldnr2222@naver.com>
2022-04-24 16:52:06 +0900

    Spring DB 1 / 01. JDBC 이해 / 06. JDBC 개발 - 수정, 삭제

--- SpringDB/SpringDB1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ SpringDB/SpringDB1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ class MemberRepositoryV0Test {
 	@Test
 	void crud() throws SQLException {
 		// save
-		Member member = new Member("memberV3", 10000);
+		Member member = new Member("memberV100", 10000);
 		repository.save(member);
 
 		// findById
@@ -28,5 +29,15 @@ class MemberRepositoryV0Test {
 		log.info("member == findMember = {}", member == findMember);
 		log.info("member equals findMember = {}", member.equals(findMember));
 		assertThat(findMember).isEqualTo(member);
+
+		// update: money 10000 -> 20000
+		repository.update(member.getMemberId(), 20000);
+		Member updatedMember = repository.findById(member.getMemberId());
+		assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+		// delete
+		repository.delete(member.getMemberId());
+		assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+			.isInstanceOf(NoSuchElementException.class);
 	}
 }
