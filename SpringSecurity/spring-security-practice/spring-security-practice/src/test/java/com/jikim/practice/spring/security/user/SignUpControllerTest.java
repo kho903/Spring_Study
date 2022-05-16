546d5fe7d3c050db430bbc683b06012947df919a
kho903 <gmldnr2222@naver.com>
2022-05-16 14:35:36 +0900

    Spring Security / 01. 개인 노트 서비스(로그인 기능) 만들기 / 04. 테스트 구현하기

---
+++ SpringSecurity/spring-security-practice/spring-security-practice/src/test/java/com/jikim/practice/spring/security/user/SignUpControllerTest.java
@@ -0,0 +1,42 @@
+package com.jikim.practice.spring.security.user;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@Transactional
+class SignUpControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(@Autowired WebApplicationContext applicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    void signup() throws Exception {
+        mockMvc.perform(
+                post("/signup").with(csrf())
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("username", "user123")
+                        .param("password", "password")
+        ).andExpect(redirectedUrl("login")).andExpect(status().is3xxRedirection());
+    }
+}
